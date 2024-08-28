6b567e07661a3c011cad4a79fd1cd1f311308635
gordoxxx <165900261+gordoxxx@users.noreply.github.com>
2024-08-28 07:58:36 -0300

    JS

---
+++ index.js
@@ -0,0 +1,39 @@
+var nome = "Murilo"
+
+let idade = 16
+
+const pi = 3.14;
+
+//
+
+let a = 5;
+
+let b = 10;
+
+let soma = a + b;
+
+console.log (soma)
+
+//
+
+let a = 5;
+
+let b = 10; 
+
+function multiplica (x, y) {
+return x * y
+}
+
+//
+
+let frutas = ['Apple', 'Banana', 'Cherry']
+console.log (frutas [1]);
+
+//
+
+let pessoa = {
+primeiroNome: 'Murilo',
+sobrenome: 'Xavier',
+age: 25
+}
+console.log(pessoa.primeiroNome);
